714fbc39099068fcb631ea8f4fbdd20991eef07e
sujith2009 <jerinsujith640@gmail.com>
2024-10-10 17:40:41 +0530

    react change module name

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+//vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"], // Replace 'module-name' with the actual module you want to externalize
-    },
-  },
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["react-router-dom"], // Replace 'module-name' with the actual module you want to externalize
+//     },
+//   },
+// });
