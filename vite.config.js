eea3d26b93c1a1d118cf3c2f0c7b5f518442930d
sujith2009 <jerinsujith640@gmail.com>
2024-10-10 17:07:53 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["module-name"], // Replace 'module-name' with the actual module you want to externalize
+    },
+  },
+});
